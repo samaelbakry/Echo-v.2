cb9b45d0f58ac3f2197063154f75a3345879b966
samaelbakry <samaabakry68@gmail.com>
2026-04-26 21:40:56 +0300

    feat: enhance AuthProtectedRoutes to handle loading state and improve user data fetching

--- src/components/protectedRoutes/AuthProtectedRoutes.tsx
+++ src/components/protectedRoutes/AuthProtectedRoutes.tsx
@@ -1,23 +1,28 @@
 import { tokenContext } from "@/context/TokenContextProvider";
 import { useUserDataQuery } from "@/hooks/useUserDataQuery/useUserDataQuery";
 import getDaysLeft from "@/lib/getDaysLeft";
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 
 const AuthProtectedRoutes = ({ children }: { children: React.ReactNode }) => {
   const { isLoggedIn } = useContext(tokenContext);
-  const {userData} = useUserDataQuery()
+  const { userData, isLoading } = useUserDataQuery();
 
-  if(isLoggedIn && userData?.createdAt){
-    const daysLeft = getDaysLeft( userData?.CreatedAt)
-    
-    if(daysLeft <= 0) {
-       return <Navigate to="/warning" replace />;
+  if (isLoading) return null; 
+
+  if (isLoggedIn) {
+    if (userData?.createdAt) {
+      const daysLeft = getDaysLeft(userData.createdAt);
+
+      if (daysLeft <= 0) {
+        return <Navigate to="/warning" replace />;
+      }
     }
-     return <Navigate to="/home" replace />;
+
+    return <Navigate to="/home" replace />;
   }
 
-  return children
+  return children;
 };
 
-export default AuthProtectedRoutes;
+export default AuthProtectedRoutes
--- src/hooks/useUserDataQuery/useUserDataQuery.tsx
+++ src/hooks/useUserDataQuery/useUserDataQuery.tsx
@@ -2,10 +2,10 @@ import { getUserData } from "@/services/userServices";
 import { useQuery} from "@tanstack/react-query";
 
 export function useUserDataQuery(){
-const { data: userData } = useQuery({
+const { data: userData , isLoading } = useQuery({
     queryKey: ["getUserData"],
     queryFn: getUserData,
     select: (data) => data?.data?.data?.user,
   });
-  return{userData}
+  return{userData , isLoading}
 }
